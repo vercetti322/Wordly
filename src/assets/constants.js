ccd17ae7dad0f9cd86ad583c53f2233fbfec08d8
vercetti322 <vercettitommy322@gmail.com>
2024-12-07 21:50:10 +0530

    registration added!

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,6 +1,45 @@
-export const demoGridData = [
-    ['C', 'A', 'B', 'L', 'E', 'X', 'T', 'E'],
-    ['D', 'A', 'I', 'M', 'I', 'N', 'G', 'S'],
-    ['S', 'T', 'A', 'R', 'L', 'I', 'N', 'K'],
-    ['N', 'T', 'E', 'L', 'O', 'S', 'E', 'R'],
-];
+export const demoGridJson = {
+    "grid": [
+        [
+            { "letter": "C", "status": 1 },
+            { "letter": "A", "status": 1 },
+            { "letter": "B", "status": 1 },
+            { "letter": "L", "status": 1 },
+            { "letter": "E", "status": 1 },
+            { "letter": "X", "status": 1 },
+            { "letter": "T", "status": 1 },
+            { "letter": "E", "status": 1 }
+        ],
+        [
+            { "letter": "D", "status": 1 },
+            { "letter": "A", "status": 1 },
+            { "letter": "I", "status": 1 },
+            { "letter": "M", "status": 1 },
+            { "letter": "I", "status": 1 },
+            { "letter": "N", "status": 1 },
+            { "letter": "G", "status": 1 },
+            { "letter": "S", "status": 1 }
+        ],
+        [
+            { "letter": "S", "status": 1 },
+            { "letter": "T", "status": 1 },
+            { "letter": "A", "status": 1 },
+            { "letter": "R", "status": 1 },
+            { "letter": "L", "status": 1 },
+            { "letter": "I", "status": 1 },
+            { "letter": "N", "status": 1 },
+            { "letter": "K", "status": 1 }
+        ],
+        [
+            { "letter": "N", "status": 0 },
+            { "letter": "T", "status": 0 },
+            { "letter": "E", "status": 0 },
+            { "letter": "L", "status": 0 },
+            { "letter": "O", "status": 0 },
+            { "letter": "S", "status": 1 },
+            { "letter": "E", "status": 1 },
+            { "letter": "R", "status": 1 }
+        ]
+    ]
+}
+
